71d631c1841d17d522016e984d34556f5c87d23b
pcodefromarch <primecoder8@gmail.com>
2025-08-30 16:16:35 +0530

    Refactor command handlers and implement SET and GET functions for RESP protocol

--- handler.go
+++ handler.go
@@ -1,7 +1,11 @@
 package main
 
-var Handlers = map[string]func([]Value) Value {
-	"PING":  ping,
+import "sync"
+
+var Handlers = map[string]func([]Value) Value{
+	"PING": ping,
+	"SET" : Set,
+	"GET" : Get,
 }
 
 func ping(args []Value) Value {
@@ -9,4 +13,36 @@ func ping(args []Value) Value {
 		return Value{typ: "string", str: "PONG"}
 	}
 	return Value{typ: "string", str: args[0].bulk}
+}
+
+var SETs = map[string]string{}
+var SETsm = sync.RWMutex{}
+
+func Set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "invalid number of arguments"}
+	}
+	key := args[0].bulk
+	val := args[1].bulk
+
+	SETsm.Lock()
+	SETs[key] = val
+	SETsm.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func Get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "invalid number of arguments"}
+	}
+
+	key := args[0].bulk
+	SETsm.Lock()
+	val, ok := SETs[key]
+	SETsm.Unlock()
+	if !ok {
+		return Value{typ: "null"}
+	}
+	return Value{typ: "bulk", bulk: val}
 }
